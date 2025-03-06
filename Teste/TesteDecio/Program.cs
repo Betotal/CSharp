fe90345d8cbc87b0675fd5909b5e73bea01867ce
Betotal <48997213+Betotal@users.noreply.github.com>
2025-03-06 08:18:14 -0300

    Ajustes

---
+++ Teste/TesteDecio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace TesteDecio {
+    class Program {
+        static void Main(string[] args) {
+
+            int a, b, soma;
+
+            Console.Write("Digite o 1º número: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2º número: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Números digitado: {a} e {b} ");
+
+            Console.WriteLine($"A soma dos números é: {soma = a + b} ");
+
+            Console.ReadLine();
+        }
+    }
+}
